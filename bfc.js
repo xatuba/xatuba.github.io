5af8904b8bf4d1ba4fb6f99f7091f8b6f0dc2089
xatuba <93212090+xatuba@users.noreply.github.com>
2021-10-26 12:58:49 -0300

    Create bfc.js

---
+++ bfc.js
@@ -0,0 +1,37 @@
+function run25FuncAsync(func){
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+    setTimeout(function(){try{eval(func)}catch(e){}}, 0);
+}
+
+$(document).ready(function() {
+  let onClick = $('#modalPlant').find('button:eq(1)').attr('onClick');
+  $('#modalPlant').find('button:eq(1)').attr('onClick','');
+  
+  $('#modalPlant').find('button:eq(1)').on('click', function(){
+    run25FuncAsync(onClick);
+  });
+});
